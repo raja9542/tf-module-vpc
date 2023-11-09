f2a004ea5c1a6240057e18df06cf6c47945f0963
raja9542 <rajasekhar1banda@gmail.com>
2023-11-09 21:32:23 +0530

    NAT gateway and Eip creation

--- main.tf
+++ main.tf
@@ -75,6 +75,23 @@ resource "aws_route_table_association" "public-rt-assoc" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "ngw-eip" {
+   domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-ngw"}
+  )
+
+#  depends_on = [aws_internet_gateway.igw]
+}
+
+
 // create EC2
 
 #data "aws_ami" "centos8" {
