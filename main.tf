b4c45647e66e21a053ac7b15f884117fb8167239
raja9542 <rajasekhar1banda@gmail.com>
2023-11-09 13:48:53 +0530

    aws routing on both vpcs

--- main.tf
+++ main.tf
@@ -29,9 +29,18 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
+# To route network traffic we need route network from both sides of vpc
+
 resource "aws_route" "default" {
   route_table_id            = aws_vpc.main.default_route_table_id # default route table id of workstation vpc
   destination_cidr_block    = "172.31.0.0/16" # default vpc ipv4 cidr (go to your vpcs there we can find it)
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
+}
+
+resource "aws_route" "default" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id # id of main route table associated with created VPC
+  destination_cidr_block    = var.cidr_block # our cidr block [10.0.0.0/16]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
 }
