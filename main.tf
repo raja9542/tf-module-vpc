80ee5d048b1a1d6d0ed23423040ba40302010672
raja9542 <rajasekhar1banda@gmail.com>
2023-11-09 19:03:38 +0530

    internet gateway and ec2

--- main.tf
+++ main.tf
@@ -32,15 +32,71 @@ resource "aws_vpc_peering_connection" "peer" {
 # To route network traffic we need route network from both sides of vpc
 
 resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id # default route table id of workstation vpc
+  route_table_id            = aws_vpc.main.default_route_table_id # created vpc route table id
   destination_cidr_block    = "172.31.0.0/16" # default vpc ipv4 cidr (go to your vpcs there we can find it)
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
 }
 
 resource "aws_route" "default_vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id # id of main route table associated with created VPC
+  route_table_id            = data.aws_vpc.default.main_route_table_id # id of default route table associated -workstation vpc
   destination_cidr_block    = var.cidr_block # our cidr block [10.0.0.0/16]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-igw"}
+  )
+}
+
+// create EC2
+
+data "aws_ami" "centos8" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = aws_subnet.main.*.id[0]
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.main.id
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
