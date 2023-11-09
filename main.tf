fbb12c85452e4a950bc57811e2a8f9c4422775e9
raja9542 <rajasekhar1banda@gmail.com>
2023-11-09 21:04:34 +0530

    public route table association

--- main.tf
+++ main.tf
@@ -69,6 +69,12 @@ resource "aws_route_table" "public" {
   )
 }
 
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
 // create EC2
 
 #data "aws_ami" "centos8" {
