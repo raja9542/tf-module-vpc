e6aa4e301c5fd7019ec88b74a610eba3e47cdb09
raja9542 <rajasekhar1banda@gmail.com>
2023-11-09 20:42:00 +0530

    vpc with 2 public & 2 Private Subnets

--- main.tf
+++ main.tf
@@ -6,20 +6,31 @@ resource "aws_vpc" "main" {
     )
 }
 
-resource "aws_subnet" "main" {
-  count      = length(var.subnets_cidr)
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnets_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    {Name = "${var.env}-subnet-${count.index+1}"}
+    {Name = "${var.env}-public-subnet-${count.index+1}"}
+  )
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-private-subnet-${count.index+1}"}
   )
 }
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_owner_id = data.aws_caller_identity.current.account_id  # rather hardcoding we can get from datasources...data.tf (aws_caller_identity datasource)
-  peer_vpc_id   = var.default_vpc_id # default vpc id or workstation vpc id
+  peer_owner_id = data.aws_caller_identity.current.account_id  #The AWS account ID of the owner of the peer VPC rather hardcoding we can get from datasources...data.tf (aws_caller_identity datasource)
+  peer_vpc_id   = var.default_vpc_id # default vpc id or workstation vpc id.. The ID of the VPC with which you are creating the VPC Peering Connection.
   vpc_id        = aws_vpc.main.id  # created vpc id
   auto_accept = true
 
@@ -29,29 +40,6 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-# To route network traffic we need route network from both sides of vpc
-
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id # created vpc route table id
-  destination_cidr_block    = "172.31.0.0/16" # default vpc ipv4 cidr (go to your vpcs there we can find it)
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-
-}
-
-resource "aws_route" "igw" {
-  route_table_id            = aws_vpc.main.default_route_table_id  # created vpc route table id
-  destination_cidr_block    = "0.0.0.0/0" # default vpc ipv4 cidr (go to your vpcs there we can find it)
-  gateway_id                =  aws_internet_gateway.igw.id
-
-}
-
-
-resource "aws_route" "default_vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id # id of default route table associated -workstation vpc
-  destination_cidr_block    = var.cidr_block # our cidr block [10.0.0.0/16]
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-
-}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -64,28 +52,28 @@ resource "aws_internet_gateway" "igw" {
 
 // create EC2
 
-data "aws_ami" "centos8" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-
-}
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id = aws_subnet.main.*.id[0]
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
-
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.main.id
+#data "aws_ami" "centos8" {
+#  most_recent      = true
+#  name_regex       = "Centos-8-DevOps-Practice"
+#  owners           = ["973714476881"]
+#
+#}
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.centos8.id
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  subnet_id = aws_subnet.main.*.id[0]
+#
+#  tags = {
+#    Name = "test-centos8"
+#  }
+#}
+#
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id = aws_vpc.main.id
 
 
   ingress {
--- vars.tf
+++ vars.tf
@@ -1,4 +1,5 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "subnets_cidr" {}
 variable "default_vpc_id" {}
+variable "public_subnets_cidr" {}
+variable "private_subnets_cidr" {}
