d81d63c97c106a4b338d4d6b5e58db5652068aa2
raja9542 <rajasekhar1banda@gmail.com>
2023-11-10 14:31:48 +0530

    subnets with specific Az's

--- main.tf
+++ main.tf
@@ -161,6 +161,14 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
