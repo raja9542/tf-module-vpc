eeb5e1c0b66867dbeb9e4fb63e9237bcd2bc02dd
raja9542 <rajasekhar1banda@gmail.com>
2023-11-11 15:52:06 +0530

    creating separate public & private subnets

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,21 @@ output "public_subnet_ids" {
 }
 
 output "one_subnet_id" {
-  value = lookup(lookup(module.public_subnets, "public", {}), "subnet_ids", [])[0]
+  value = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
   //value = module.public_subnets["public"]["subnet_ids"][0]
 }
+
+#out = {
+#  "main" = { -----this is from root module
+#    "public_subnet_ids" = { ===from vpc module
+#      "public" = { ---from public_subnets module
+#        "subnet_ids" = [
+#          "subnet-00adbe860e3616783",
+#          "subnet-0a473f1120f11aaf8",
+#        ]
+#      }
+#    }
+#    "vpc_id" = "vpc-0496ca9654fa61564"
+#    "vpc_peering_connection_id" = "pcx-02e186f1ce4a8d3cf"
+#  }
+#}
