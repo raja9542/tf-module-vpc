807dbf069ebefbb48a880b92c86b0a3494b5945a
raja9542 <rajasekhar1banda@gmail.com>
2023-11-11 13:57:34 +0530

    internet gw attaching to public subnet only

--- subnets.tf
+++ subnets.tf
@@ -9,8 +9,8 @@ module "subnets" {
   cidr_block                 = each.value.cidr_block
   name                       = each.value.name
   # lookup function looks for a particular value if its there it gives that value else false
-  internet_gw                = lookup(each.value, "internet_gw ", false )
-#  nat_gw                    = lookup(each.value, "nat_gw", false )
+  internet_gw                = lookup(each.value, "internet_gw", false)
+#  nat_gw                    = lookup(each.value, "nat_gw", false)
 
   vpc_id                     = aws_vpc.main.id
   vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
