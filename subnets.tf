b9b3f0acd81be7648869a2349d1c26c50d38b43c
raja9542 <rajasekhar1banda@gmail.com>
2023-11-11 16:04:52 +0530

    created nat in public subnet & attaching to private subnets

--- subnets.tf
+++ subnets.tf
@@ -10,12 +10,13 @@ module "public_subnets" {
   name                       = each.value.name
   # lookup function looks for a particular value if its there it gives that value else false
   internet_gw                = lookup(each.value, "internet_gw", false)
-  #  nat_gw                    = lookup(each.value, "nat_gw", false)
+  nat_gw                    = lookup(each.value, "nat_gw", false)
 
   vpc_id                     = aws_vpc.main.id
   vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
   common_tags                = local.common_tags
   gateway_id                 = aws_internet_gateway.gw.id
+  nat_gw_id                  = aws_nat_gateway.ngw.id
 }
 
 module "private_subnets" {
